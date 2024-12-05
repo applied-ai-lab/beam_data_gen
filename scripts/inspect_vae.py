642d2ba3f2f889c4d0926248b4c91d12898376b8
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2024-12-05 17:03:44 +0000

    feat: add script to visualise trained model

--- scripts/inspect_vae.py
+++ scripts/inspect_vae.py
@@ -1,8 +1,14 @@
 import argparse
 
 import torch
+import numpy as np
+import mujoco
+import mujoco.viewer
+from scipy.spatial.transform import Rotation as R
 
 from vae_planner.argparse_yaml_loader.yaml_loader import YamlLoader
+from vae_planner.models.encoder_base import EncoderBase
+
 from beam_data_gen.models.beam_dataset import BeamDataset, ProcessData
 from beam_data_gen.models.beam_vae_params import BeamVaeParams
 from beam_data_gen.models.beam_train_params import TrainParams
@@ -20,24 +26,62 @@ def main():
     vae_params = BeamVaeParams(vae_args)
     train_params = TrainParams(train_args)
     
+    data_processor = ProcessData(np.array([0.6, 0.6, 0.08]))
+    
     with torch.no_grad():
         model = BeamVae(vae_params, 
                     train_params,
-                    BeamEncoder,
+                    EncoderBase,
                     BeamDecoder,
                     BeamGraphClassifier).to(vae_params.device)
         
         model.load_state_dict(torch.load(vae_params.in_path))
         
         latents = LatentVarsBase()
-        latents.z = torch.zeros([1000, vae_params.latent_dim], dtype=torch.float32).to(vae_params.device)
         
-        out_pred = model.decoder(latents, None)
-        out_graph = model.classifier(latents.z)
+        m = mujoco.MjModel.from_xml_path('resources/configs/three_beams.xml')
+        d = mujoco.MjData(m)
         
-        print(torch.sigmoid(out_graph).round())
+        with mujoco.viewer.launch_passive(m, d) as viewer:
+            
+            # Start loop and sample pose
+            while viewer.is_running():
+            
+                
+                latents.z = torch.randn([1, vae_params.latent_dim], dtype=torch.float32).to(vae_params.device)
         
-        import pdb; pdb.set_trace()   
+                out_pred = model.decoder(latents, None)
+                out_graph = model.classifier(latents.z)
+                
+                print(torch.sigmoid(out_graph).round())
+                
+                denorm_out = data_processor.denorm_output(out_pred.x_pred)
+                
+                # Set position
+                d.qpos[0:3] = denorm_out[0, 0:3].cpu().numpy()
+                d.qpos[7:10] = denorm_out[0, 5:8].cpu().numpy()
+                d.qpos[14:17] = denorm_out[0, 9:12].cpu().numpy()
+                
+                # Set orientation
+                l1_z = R.from_euler("xyz", [0, 0, denorm_out[0, 3].cpu().numpy()])
+                l2_z = R.from_euler("xyz", [0, 0, denorm_out[0, 8].cpu().numpy()])
+                pa_z = R.from_euler("xyz", [0, 0, denorm_out[0, 13].cpu().numpy()])
+                
+                d.qpos[4:7]   = l1_z.as_quat()[0:3]
+                d.qpos[3]   = l1_z.as_quat()[3]
+                d.qpos[11:14] = l2_z.as_quat()[0:3]
+                d.qpos[10] = l2_z.as_quat()[3]
+                d.qpos[18:21] = pa_z.as_quat()[0:3]
+                d.qpos[17] = pa_z.as_quat()[3]            
+                
+                # mj_step can be replaced with code that also evaluates
+                # a policy and applies a control signal before stepping the physics.
+                mujoco.mj_step(m, d)    
+
+                # Pick up changes to the physics state, apply perturbations, update options from GUI.
+                viewer.sync()
+        
+                import pdb; pdb.set_trace()   
 
 
     return 0

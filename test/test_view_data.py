199fc8e80ac055769f01fdc95343f9ed0d1878a8
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2025-01-24 07:15:24 +0000

    feat: update view data for robot bem model

--- test/test_view_data.py
+++ test/test_view_data.py
@@ -2,21 +2,29 @@ import mujoco
 import mujoco.viewer
 import numpy as np
 import torch
+from scipy.spatial.transform import Rotation as R
 
 from beam_data_gen.models.beam_dataset import BeamDataset, ProcessData
+from beam_data_gen.simulator.beam_robot_sim import BeamRobotSim
 
 
 def main():
-    m = mujoco.MjModel.from_xml_path('resources/configs/three_beams.xml')
+    m = mujoco.MjModel.from_xml_path('resources/configs/robot_and_beams.xml')
     d = mujoco.MjData(m)
+    
+    # Beam robot sim
+    sim = BeamRobotSim()
     
     # Process data
     process_data = ProcessData(np.array([0.6, 0.6, 0.08]))
-    poses, flat_adj = process_data("data/graphs/", ["l_beam_1", "l_beam_2", "l_pin_A"])
+    poses, flat_adj = process_data("data/robot_graphs/", ["robot_left_hand", "robot_right_hand", "l_beam_1", "l_beam_2", "l_pin_A"])
 
     poses = torch.tensor(poses, dtype=torch.float32, device=torch.device("cuda"))
     # Create dataset and dataloaders
     denorm_out = process_data.denorm_output(poses)
+    
+    beams_out = denorm_out[:, 2*5:]
+    robot_out = denorm_out[:, 0:2*5]
     
     k = 0
     
@@ -29,14 +37,40 @@ def main():
             # a policy and applies a control signal before stepping the physics.
             mujoco.mj_step(m, d)
             
-            d.qpos[0:3] = denorm_out[k, 0:3].cpu().numpy()
-            d.qpos[7:10] = denorm_out[k, 5:8].cpu().numpy()
-            d.qpos[14:17] = denorm_out[k, 10:13].cpu().numpy()
+            # Set position
+            d.qpos[0:3] = beams_out[k, 0:3].cpu().numpy()
+            d.qpos[7:10] = beams_out[k, 5:8].cpu().numpy()
+            d.qpos[14:17] = beams_out[k, 9:12].cpu().numpy()
+            
+            d.qpos[21:24] = robot_out[k, 0:3].cpu().numpy()
+            d.qpos[28:31] = robot_out[k, 5:8].cpu().numpy()
+            
+            # Set orientation
+            l1_z = R.from_euler("xyz", [0, 0, beams_out[k, 3].cpu().numpy()])
+            l2_z = R.from_euler("xyz", [0, 0, beams_out[k, 8].cpu().numpy()])
+            pa_z = R.from_euler("xyz", [0, 0, beams_out[k, 13].cpu().numpy()])
+            
+            robot_left = R.from_euler("xyz", [0, 0, robot_out[k, 3].cpu().numpy()])
+            robot_right = R.from_euler("xyz", [0, 0, robot_out[k, 8].cpu().numpy()])
+            
+            d.qpos[4:7]   = l1_z.as_quat()[0:3]
+            d.qpos[3]   = l1_z.as_quat()[3]
+            d.qpos[11:14] = l2_z.as_quat()[0:3]
+            d.qpos[10] = l2_z.as_quat()[3]
+            d.qpos[18:21] = pa_z.as_quat()[0:3]
+            d.qpos[17] = pa_z.as_quat()[3]    
+            
+            d.qpos[25:28] = robot_left.as_quat()[0:3]
+            d.qpos[24] = robot_left.as_quat()[3]
+                    
+            d.qpos[32:35] = robot_right.as_quat()[0:3]
+            d.qpos[31] = robot_right.as_quat()[3]
+            
+            print(flat_adj[k, :].reshape(5, 5))
             
             import pdb
             pdb.set_trace()
             
-
             # Pick up changes to the physics state, apply perturbations, update options from GUI.
             viewer.sync()
             

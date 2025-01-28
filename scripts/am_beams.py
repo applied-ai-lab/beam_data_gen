078bfe7e0a17c9906e227c09cb0e0194d7e6095a
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2025-01-28 19:00:27 +0000

    feat: plot latent space for hands and beams

--- scripts/am_beams.py
+++ scripts/am_beams.py
@@ -1,5 +1,7 @@
 import argparse
 import time
+import copy
+import os
 
 import torch
 from torch import nn
@@ -78,111 +80,122 @@ def main():
     
     print(latent_dims)
     
-    latents_for_plotting.z[:, latent_dims[2]] = x
-    latents_for_plotting.z[:, latent_dims[3]] = y
+    for i in range(latent_dims.shape[0]):
+        j = copy.deepcopy(i)
+        while j < latent_dims.shape[0]:
+            latents_for_plotting.z[:, latent_dims[i]] = x
+            latents_for_plotting.z[:, latent_dims[j]] = y
 
-    # Classify the graphs      
-    graphs_for_plotting = torch.sigmoid(model.classifier(latents_for_plotting.z)).round()
-    latent_inspector.plot_latents(x, y, graphs_for_plotting[:, :, :])
-    plt.show()
-        
-    # AM for ls
-    latents = LatentVarsBase()
-    
-    m = mujoco.MjModel.from_xml_path('resources/configs/three_beams.xml')
-    d = mujoco.MjData(m)
-    
-    latents.z = 2.0 * torch.ones([1, vae_params.latent_dim], dtype=torch.float32).to(vae_params.device).requires_grad_()
-    latent_list = []
-    
-    grad_features = 100 * torch.ones(latents.z.shape, dtype=torch.float32).to(vae_params.device)
-    grad_list = []
-    
-    out_pred = model.decoder(latents, None)
-    out_graph = model.classifier(latents.z)
+            # Classify the graphs      
+            graphs_for_plotting = torch.sigmoid(model.classifier(latents_for_plotting.z)).round()
+            title = f"Latent dim {i} and {j}, VAE {os.path.basename(vae_params.in_path)}"
+            fig, axes = latent_inspector.plot_latents(x, y, graphs_for_plotting[:, :, :], title)
             
-    denorm_out = data_processor.denorm_output(out_pred.x_pred)
-    
-    # Graph Target
-    adj_mat = l_connected_robot.A
-    graph_target = torch.tensor([adj_mat], dtype=torch.float32)
-    graph_target = graph_target.to(vae_params.device)
-    
-    print(adj_mat)
-    print(torch.sigmoid(out_graph).round())
-    
-    # # Polar latents [R, theta]
-    # z_polar = torch.tensor([torch.sqrt(torch.square(latents.z[:, latent_dims[1]]) + 
-    #                                    torch.square(latents.z[:, latent_dims[0]])).item(),
-    #                         torch.atan2(latents.z[:, latent_dims[1]], latents.z[:, latent_dims[0]]).item()],
-    #                        dtype=torch.float32).requires_grad_()
-    
-    termination_criteria = 0.45
-    
-    loss_func = nn.BCEWithLogitsLoss()
-    
-    loss = loss_func(out_graph, graph_target)
-    
-    # Visualisation runs
-    with mujoco.viewer.launch_passive(m, d) as viewer:
+            file_path = os.path.join('figures', 'latent_space', f'VAE_11_latent_{i}_{j}')
+            print(file_path)
+            plt.savefig(file_path)
+            
+            plt.close(fig)
+            
+            j += 1
         
-        input("continue")
+    # # AM for ls
+    # latents = LatentVarsBase()
+    
+    # m = mujoco.MjModel.from_xml_path('resources/configs/three_beams.xml')
+    # d = mujoco.MjData(m)
+    
+    # latents.z = 2.0 * torch.ones([1, vae_params.latent_dim], dtype=torch.float32).to(vae_params.device).requires_grad_()
+    # latent_list = []
+    
+    # grad_features = 100 * torch.ones(latents.z.shape, dtype=torch.float32).to(vae_params.device)
+    # grad_list = []
+    
+    # out_pred = model.decoder(latents, None)
+    # out_graph = model.classifier(latents.z)
+            
+    # denorm_out = data_processor.denorm_output(out_pred.x_pred)
+    
+    # # Graph Target
+    # adj_mat = l_connected_robot.A
+    # graph_target = torch.tensor([adj_mat], dtype=torch.float32)
+    # graph_target = graph_target.to(vae_params.device)
+    
+    # print(adj_mat)
+    # print(torch.sigmoid(out_graph).round())
+    
+    # # # Polar latents [R, theta]
+    # # z_polar = torch.tensor([torch.sqrt(torch.square(latents.z[:, latent_dims[1]]) + 
+    # #                                    torch.square(latents.z[:, latent_dims[0]])).item(),
+    # #                         torch.atan2(latents.z[:, latent_dims[1]], latents.z[:, latent_dims[0]]).item()],
+    # #                        dtype=torch.float32).requires_grad_()
+    
+    # termination_criteria = 0.45
+    
+    # loss_func = nn.BCEWithLogitsLoss()
+    
+    # loss = loss_func(out_graph, graph_target)
+    
+    # # Visualisation runs
+    # with mujoco.viewer.launch_passive(m, d) as viewer:
         
-        step_start = time.time()
+    #     input("continue")
         
-        # Start loop and sample pose
-        while viewer.is_running():
-            while torch.norm(loss) > termination_criteria:
-                # Loss                
-                out_graph = model._classifier.forward(latents.z)
-                loss = loss_func(out_graph, graph_target)
+    #     step_start = time.time()
+        
+    #     # Start loop and sample pose
+    #     while viewer.is_running():
+    #         while torch.norm(loss) > termination_criteria:
+    #             # Loss                
+    #             out_graph = model._classifier.forward(latents.z)
+    #             loss = loss_func(out_graph, graph_target)
                 
-                print(torch.sigmoid(out_graph).round())
+    #             print(torch.sigmoid(out_graph).round())
                 
-                # Graph features
-                grad_features = grad(outputs=loss, inputs=latents.z, retain_graph=True)[0]
-                grad_list.append(grad_features.detach().cpu().numpy().squeeze())
+    #             # Graph features
+    #             grad_features = grad(outputs=loss, inputs=latents.z, retain_graph=True)[0]
+    #             grad_list.append(grad_features.detach().cpu().numpy().squeeze())
                     
-                latents.z = latents.z - 1.0e-3 * grad_features
-                latent_list.append(latents.z.detach().cpu().numpy().squeeze())
+    #             latents.z = latents.z - 1.0e-3 * grad_features
+    #             latent_list.append(latents.z.detach().cpu().numpy().squeeze())
                 
-                out_pred = model.decoder(latents, None)
-                out_graph = model.classifier(latents.z)
+    #             out_pred = model.decoder(latents, None)
+    #             out_graph = model.classifier(latents.z)
                 
-                denorm_out = data_processor.denorm_output(out_pred.x_pred)[:, 10:]
+    #             denorm_out = data_processor.denorm_output(out_pred.x_pred)[:, 10:]
                 
-                # Set position
-                d.qpos[0:3] = denorm_out[0, 0:3].cpu().detach().numpy()
-                d.qpos[7:10] = denorm_out[0, 5:8].cpu().detach().numpy()
-                d.qpos[14:17] = denorm_out[0, 10:13].cpu().detach().numpy()
+    #             # Set position
+    #             d.qpos[0:3] = denorm_out[0, 0:3].cpu().detach().numpy()
+    #             d.qpos[7:10] = denorm_out[0, 5:8].cpu().detach().numpy()
+    #             d.qpos[14:17] = denorm_out[0, 10:13].cpu().detach().numpy()
                 
-                # Set orientation
-                l1_z = R.from_euler("xyz", [0, 0, denorm_out[0, 3].cpu().detach().numpy()])
-                l2_z = R.from_euler("xyz", [0, 0, denorm_out[0, 8].cpu().detach().numpy()])
-                pa_z = R.from_euler("xyz", [0, 0, denorm_out[0, 13].cpu().detach().numpy()])
+    #             # Set orientation
+    #             l1_z = R.from_euler("xyz", [0, 0, denorm_out[0, 3].cpu().detach().numpy()])
+    #             l2_z = R.from_euler("xyz", [0, 0, denorm_out[0, 8].cpu().detach().numpy()])
+    #             pa_z = R.from_euler("xyz", [0, 0, denorm_out[0, 13].cpu().detach().numpy()])
                 
-                d.qpos[4:7]   = l1_z.as_quat()[0:3]
-                d.qpos[3]   = l1_z.as_quat()[3]
-                d.qpos[11:14] = l2_z.as_quat()[0:3]
-                d.qpos[10] = l2_z.as_quat()[3]
-                d.qpos[18:21] = pa_z.as_quat()[0:3]
-                d.qpos[17] = pa_z.as_quat()[3]            
+    #             d.qpos[4:7]   = l1_z.as_quat()[0:3]
+    #             d.qpos[3]   = l1_z.as_quat()[3]
+    #             d.qpos[11:14] = l2_z.as_quat()[0:3]
+    #             d.qpos[10] = l2_z.as_quat()[3]
+    #             d.qpos[18:21] = pa_z.as_quat()[0:3]
+    #             d.qpos[17] = pa_z.as_quat()[3]            
                 
-                # mj_step can be replaced with code that also evaluates
-                # a policy and applies a control signal before stepping the physics.
-                mujoco.mj_step(m, d)    
+    #             # mj_step can be replaced with code that also evaluates
+    #             # a policy and applies a control signal before stepping the physics.
+    #             mujoco.mj_step(m, d)    
 
-                # Pick up changes to the physics state, apply perturbations, update options from GUI.
-                viewer.sync()   
+    #             # Pick up changes to the physics state, apply perturbations, update options from GUI.
+    #             viewer.sync()   
                 
-                # Rudimentary time keeping, will drift relative to wall clock.
-                time.sleep(0.1)
+    #             # Rudimentary time keeping, will drift relative to wall clock.
+    #             time.sleep(0.1)
             
-            # Stack gradients
-            gradient_traj = np.vstack(grad_list)
-            latent_traj = np.vstack(latent_list)
+    #         # Stack gradients
+    #         gradient_traj = np.vstack(grad_list)
+    #         latent_traj = np.vstack(latent_list)
             
-            import pdb; pdb.set_trace()
+    #         import pdb; pdb.set_trace()
 
 
     return 0

0655c90149ccd4228551af464f44c3161a5c60f3
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2025-02-22 21:57:26 +0000

    feat: add simulator helpers

--- src/beam_data_gen/simulator/beam_robot_sim.py
+++ src/beam_data_gen/simulator/beam_robot_sim.py
@@ -1,13 +1,15 @@
 from typing import List
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 from beam_data_gen.beam_impl.robot_graph import RobotGraph
+from beam_data_gen.models.beam_dataset import ProcessData
 
 
 class BeamRobotSim:
-    def __init__(self):
-        pass
+    def __init__(self, process_data: ProcessData):
+        self._data_processor = process_data
     
     def pose_to_q(self, trans, rot):
         pose = np.zeros(7)
@@ -44,3 +46,39 @@ class BeamRobotSim:
             elif not ramp_graph.graph.has_edge(geom_to_name[geom1], geom_to_name[geom2]):
                 return True
         return collision
+    
+    def decode_x(self, data, x_pred):
+        denorm_output = self._data_processor.denorm_output(x_pred)[:, :]
+        denorm_out = denorm_output[:, 2*5:]
+        robot_out = denorm_output[:, 0:2*5]
+        
+        # Set position
+        data.qpos[0:3] = denorm_out[0, 0:3].cpu().detach().numpy()
+        data.qpos[7:10] = denorm_out[0, 5:8].cpu().detach().numpy()
+        data.qpos[14:17] = denorm_out[0, 10:13].cpu().detach().numpy()
+        
+        data.qpos[21:24] = robot_out[0, 0:3].cpu().detach().numpy()
+        data.qpos[28:31] = robot_out[0, 5:8].cpu().detach().numpy()
+        
+        # Set orientation
+        l1_z = R.from_euler("xyz", [0, 0, denorm_out[0, 3].cpu().detach().numpy()])
+        l2_z = R.from_euler("xyz", [0, 0, denorm_out[0, 8].cpu().detach().numpy()])
+        pa_z = R.from_euler("xyz", [0, 0, denorm_out[0, 13].cpu().detach().numpy()])
+        
+        robot_left = R.from_euler("xyz", [0, 0, robot_out[0, 3].cpu().detach().numpy()])
+        robot_right = R.from_euler("xyz", [0, 0, robot_out[0, 8].cpu().detach().numpy()])
+        
+        data.qpos[4:7]   = l1_z.as_quat()[0:3]
+        data.qpos[3]   = l1_z.as_quat()[3]
+        data.qpos[11:14] = l2_z.as_quat()[0:3]
+        data.qpos[10] = l2_z.as_quat()[3]
+        data.qpos[18:21] = pa_z.as_quat()[0:3]
+        data.qpos[17] = pa_z.as_quat()[3]    
+        
+        data.qpos[25:28] = robot_left.as_quat()[0:3]
+        data.qpos[24] = robot_left.as_quat()[3]
+                
+        data.qpos[32:35] = robot_right.as_quat()[0:3]
+        data.qpos[31] = robot_right.as_quat()[3]
+        return
+        

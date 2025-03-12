d27aa16e6c1c4cdd773a0684dc09651c8f819b72
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2025-03-12 23:58:24 +0000

    feat: add test for perturb pose

--- test/test_perturb_poses.py
+++ test/test_perturb_poses.py
@@ -4,8 +4,10 @@ import numpy as np
 import mujoco
 import mujoco.viewer
 from matplotlib import pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
-from beam_data_gen.transformations.pose_sampler import PoseSampler
+from beam_data_gen.transformations.pose_sampler import PoseSampler, PoseSamplerParams
+from beam_data_gen.beam_sampler import BeamSampler
 from beam_data_gen.beam_impl.robot_graph import (l_connected_robot, l_pin_removed_robot, l_disconnected_robot, RobotGraph)
 from beam_data_gen.simulator.beam_robot_sim import BeamRobotSim
 from beam_data_gen.models.beam_dataset import ProcessData
@@ -13,16 +15,23 @@ from beam_data_gen.models.beam_dataset import ProcessData
 
 def main():
     
-    sampler = PoseSampler()
+    pose_sampler = PoseSampler()
+    beam_sampler = BeamSampler(np.array([0.6, 0.6, 0.08]))
     
     num_samples = 100
     time_span = 5
     dt = time_span / num_samples
     # Generate smooth velocities
-    t, velocity = sampler.generate_smooth_velocities(num_samples, time_span, 42)
+    t, velocity = pose_sampler.generate_smooth_velocities(num_samples, time_span, 42)
+    velocity *= np.array([1, 1, 0, 0, 0, 1])
+    
+    params = PoseSamplerParams(num_samples, time_span, None, np.array([1, 1, 0, 0, 0, 1]))
     
     # Get initial poses from the graph
     graph = l_connected_robot
+    # Apply hand
+    graph.add_hand("robot_left_hand", "l_beam_1")
+    # Get nodes and data
     nodes_and_data = graph.node_lst
     
     # Find beams
@@ -32,10 +41,17 @@ def main():
     
     # Apply velocitiies    
     modified_poses = np.array([
-        sampler.apply_velocities_to_poses(init_poses[k], velocity, dt)
+        pose_sampler.apply_velocities_to_poses(init_poses[k], velocity, dt)
         for k in range(len(init_poses))
-    ])   
+    ])
     
+    def sample_func(index_args):
+        index = index_args[0]
+        trans, rot = modified_poses[0, index, 0:3], R.from_quat(modified_poses[0, index, 3:7])
+        index += 1
+        index = index % num_samples
+        return trans, rot       
+        
     # Visualise the trajectories
     m = mujoco.MjModel.from_xml_path('resources/configs/robot_and_beams.xml')
     d = mujoco.MjData(m)
@@ -44,13 +60,14 @@ def main():
     data_processor = ProcessData(np.array([0.6, 0.6, 0.08]))
     sim = BeamRobotSim(data_processor)
     
+    beam_sampler.move_hands(graph, params)
+    
     counter = 0
         
     with mujoco.viewer.launch_passive(m, d) as viewer:
-        while viewer.is_running():            
-            # Set graph poses using modified poses
-            nodes_data = graph.node_lst
-            nodes_data['l_beam_1']['pose'].from_pose_quat(modified_poses[0, counter, :])            
+        while viewer.is_running() and counter < num_samples:            
+            # Set pose using the trajectory 
+            beam_sampler.set_pose_with_traj(graph, counter)
             
             pose_dict = sim.graph_to_pose_dict(graph)
             d.qpos = sim.set_q(pose_dict)

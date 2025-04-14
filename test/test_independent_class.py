8c89351d58ebbc80e34c0329ecbcf76bb1cea48a
Alexander Mitchell <alex.l.mitchell@btinternet.com>
2025-04-14 10:29:36 +0000

    feat: update test for independent classifier

--- test/test_independent_class.py
+++ test/test_independent_class.py
@@ -1,8 +1,17 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim 
 
 from beam_data_gen.models.classifiers.independent_classifier import IndependentClassifier
 from beam_data_gen.models.parameters.beam_train_params import TrainParams
 from beam_data_gen.models.parameters.beam_vae_params import BeamVaeParams
+
+
+def print_params(model):
+    for name, param in model.named_parameters():
+            print(name, param.shape)
+            print(param.data.clone())  # or param.data for raw values
+    return
 
 
 def main():
@@ -17,14 +26,28 @@ def main():
     # Create model
     classifier = IndependentClassifier(beam_params).to(beam_params.device)
     
-    no_predictions = classifier.no_predictions
+    optimizer = optim.Adam(classifier.parameters(), lr=train_params.lr)
     
     batch_size = 2
     
-    input = torch.zeros([batch_size, beam_params.latent_dim], dtype=torch.float32).to(beam_params.device)
+    input = torch.ones([batch_size, beam_params.latent_dim], dtype=torch.float32).to(beam_params.device)
     
-    out_graph = classifier(input)
+    target = torch.ones([batch_size, 3, 3], dtype=torch.float32).to(beam_params.device)
     
+    loss = nn.MSELoss()
+    
+    print_params(classifier)
+
+    for _ in range(1):
+        out_graph = classifier(input)
+        
+        loss_value = loss(target, out_graph)
+        loss_value.backward()    
+        
+        optimizer.step()
+        
+        print_params(classifier)
+        
     import pdb; pdb.set_trace()
     
     return 0
